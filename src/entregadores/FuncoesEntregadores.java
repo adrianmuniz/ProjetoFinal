9ee4ca9d33f5c8e021c8c2b6164c0ac4906e889a
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-13 19:37:05 -0300

    Projeto Inicial

--- src/entregadores/FuncoesEntregadores.java
+++ src/entregadores/FuncoesEntregadores.java
@@ -2,4 +2,10 @@ package entregadores;
 
 public class FuncoesEntregadores {
 
+	public static boolean login (String usuario, String senha) {
+		if(usuario.equalsIgnoreCase("Admin") && senha.equalsIgnoreCase("123")) {
+			return true;
+		}
+		return false;
+	}
 }
