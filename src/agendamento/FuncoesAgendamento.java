f704e21b36ef4c30ec1d8216fe4582f2fd24f38d
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2021-08-17 09:13:16 -0300

    Criando e exibindo o menu

--- src/agendamento/FuncoesAgendamento.java
+++ src/agendamento/FuncoesAgendamento.java
@@ -9,6 +9,13 @@ public class FuncoesAgendamento {
 		return false;
 	}
 	
+	public static void exibirMenu() {
+		System.out.println("ESCOLHA UMA DAS OPÇÕES A BAIXO!");
+		System.out.println("1 - Adicionar horários disponiveis");
+		System.out.println("2 - Agendar horário");
+		System.out.println("3 - Exibir agenda completa");
+		System.out.println("4 - Sair do programa");
+	}
 	
 
 }
